5f3dad01af2b0c252769bf15724095c556e169c7
agazda1 <agazda1@student.cccd.edu>
2017-09-19 16:58:51 -0700

    Added new strings, colors, and app icon.

--- app/src/main/java/edu/orangecoastcollege/cs273/agazda1/paintestimator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/agazda1/paintestimator/MainActivity.java
@@ -1,13 +1,106 @@
 package edu.orangecoastcollege.cs273.agazda1.paintestimator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Member Variables
+    private EditText mLengthEditText;
+    private EditText mWidthEditText;
+    private EditText mHeightEditText;
+
+    private EditText mWindowsEditText;
+    private EditText mDoorsEditText;
+    private TextView mGallonsTextView;
+
+    //member var for model
+    private InteriorRoom mRoom = new InteriorRoom();
+
+    //member var for shared preferences
+    private SharedPreferences mPrefs;
+
+    private void initializeViews()
+    {
+        mLengthEditText = (EditText) findViewById(R.id.lengthEditText);
+        mWidthEditText = (EditText) findViewById(R.id.widthEditText);
+        mHeightEditText = (EditText) findViewById(R.id.heightEditText);
+        mWindowsEditText = (EditText) findViewById(R.id.windowEditText);
+        mDoorsEditText = (EditText) findViewById(R.id.doorEditText);
+        mGallonsTextView = (TextView) findViewById(R.id.gallonsTextView);
+        // TO DO FINISH INITIALIZING
+    }
+
+    private void loadSharedPreferences()
+    {
+        mPrefs = getSharedPreferences("edu.orangecoastcollege.cs273.agazda1.PaintEstimator", MODE_PRIVATE);
+        if (mPrefs != null)
+        {
+            // Load all the room information
+            mRoom.setDoors(mPrefs.getInt("doors", 0));
+            mDoorsEditText.setText(String.valueOf(mRoom.getDoors())); //very improtant to convert to string!
+            mRoom.setWindows(mPrefs.getInt("windows", 0));
+            mWindowsEditText.setText(String.valueOf(mRoom.getWindows()));
+            mRoom.setHeight(mPrefs.getFloat("height", 0.0f));
+            mHeightEditText.setText(String.valueOf(mRoom.getHeight()));
+            mRoom.setLength(mPrefs.getFloat("length", 0.0f));
+            mLengthEditText.setText(String.valueOf(mRoom.getLength()));
+            mRoom.setWidth(mPrefs.getFloat("width", 0.0f));
+            mWidthEditText.setText(String.valueOf(mRoom.getWidth()));
+
+        }
+    }
+
+    private void saveSharedPreferences()
+    {
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.clear();
+        editor.putFloat("length", mRoom.getLength());
+        editor.putFloat("height", mRoom.getHeight());
+        editor.putFloat("width", mRoom.getWidth());
+        editor.putInt("doors", mRoom.getDoors());
+        editor.putInt("windows", mRoom.getWindows());
+        // save changes
+        editor.commit();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+        loadSharedPreferences();
     }
+
+    protected void computeGallons(View v)
+    {
+        //update model first, then calculate FINISH THIS
+        mRoom.setLength(Float.parseFloat(mLengthEditText.getText().toString()));
+        mRoom.setWidth(Float.parseFloat(mLengthEditText.getText().toString()));
+        mRoom.setHeight(Float.parseFloat(mLengthEditText.getText().toString()));
+        mRoom.setDoors(Integer.parseInt(mDoorsEditText.getText().toString()));
+        mRoom.setWindows(Integer.parseInt(mLengthEditText.getText().toString()));
+
+
+        mGallonsTextView.setText(getString(R.string.interior_surface_area) + mRoom.totalSurfaceArea()
+        + "\n" + getString(R.string.gallons_needed_text) + mRoom.gallonsOfPaintRequired());
+        saveSharedPreferences();
+    }
+
+    protected void goToHelp(View v)
+    {
+        //construct an explicit intent to go to helpactivity
+        // Intent: specify where it starts (context) and where its going (next Activity)
+        Intent helpIntent = new Intent(this, HelpActivity.class);
+        helpIntent.putExtra("gallons", mRoom.gallonsOfPaintRequired());
+        startActivity(helpIntent);
+    }
+
 }
